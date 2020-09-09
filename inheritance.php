af103725bb095d2f8994591908510eb6eecdb7b8
Hailey Blankenship <you@example.com>
2020-09-09 16:50:30 -0400

    Editing inheritance.php

--- inheritance.php
+++ inheritance.php
@@ -1,22 +1,36 @@
 <?php
 Class Dogs {
-    var $fur_length ;
+    var $fur_length;
+    var $fur_color;
+    var $eye_color;
+    var $legs = 4;
+    
 
-    function dog_fur() {
-        return $this->fur_length;
+    function dog_desc() {
+        return $this->fur_color . " " . $this->fur_length . " fur and " . $this->eye_color . " eyes"; 
+    }
+
+    function legs() {
+        return "Dogs have " . $this->legs . " legs."; 
     }
 }
 
 class LargeDogs extends Dogs {
     var $breed;
+    var $name;
+    var $age;
 
-    function dog_desc () {
-        return $this->breed;
+    function about_dog() {
+        return $this->name . " is a " . $this->age . " " . $this->breed;
     }
 }
 
 $d = new LargeDogs;
 $d->fur_length = 'long';
+$d->fur_color = "white and black";
+$d->eye_color = "blue";
 $d->breed = 'Huskey';
+$d->name = "Thunder";
+$d->age = "2 year old";
 
-echo $d->dog_desc() . "'s have " . $d->dog_fur() . " fur";
+echo $d->about_dog() . " with " . $d->dog_desc() . ".";
